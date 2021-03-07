c3b7c7c97fd516bb30be9e268caff9f2eaa7c789
zeffo <phatakaman@gmail.com>
2021-03-07 18:49:04 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # pytemplate
 A templating system for MS word. This is my CS Final Project, wish me luck :D
+
+
+# Usage:
+Command Line utility:
+
+
+```
+
---
+++ main.py
@@ -0,0 +1,101 @@
+
+""" 
+MS Word Automated Templating
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+A Templating system in Python3 for Microsoft Word
+
+For information on usage, check the readme (README.md)
+To report bugs or give feedback, please write to le32@myshala.com
+All feedback is appreciated.
+
+"""
+
+import csv
+from docxtpl import DocxTemplate
+from sys import argv
+import threading
+from time import perf_counter
+
+
+class Reader(object):
+    ''' Class to help parse the CSV data. '''
+    def __init__(self, target):
+        self.target = target
+
+    def __enter__(self):
+        self.f = open(self.target)
+        self.reader = csv.reader(self.f)
+        self.data = [row for row in self.reader]
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        self.f.close()
+
+    @property
+    def fields(self):
+        return self.data[0]
+        
+
+def main() -> None:
+    ''' Main function that handles and coordinated all other helper functions. '''
+    _data, template, output = argv[1:]
+        
+    with Reader(_data) as reader:
+        fields = reader.fields
+        data = reader.data[1:]
+    for index, row in enumerate(data):
+        print(index, row, fields)
+        context = dict(zip(fields, row))
+        doc = DocxTemplate(template)
+        doc.render(context)
+        thread = threading.Thread(target=doc.save, args=(f'{output}/{index}.docx',))
+        thread.start()
+    
+
+if __name__ == '__main__':
+    start = perf_counter()
+    main()
+    end = perf_counter()
+    print(f'Finished with {round(end-start, 2)}s')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
